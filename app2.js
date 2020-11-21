d23e1c9cfed8d93f1c049fb5b2c99a5003ead013
celiocss07 <celiocss07@gmail.com>
2020-11-21 11:36:41 +0100

    Começando de novo(QUERAPIDOANGOLA)

--- app2.js
+++ app2.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 
 const product = {}
 var indexGlobal = 0;
-  
+
 app.get("", (req, res) => {
 
     res.json("Hello, World!")
@@ -14,110 +14,120 @@ app.get("", (req, res) => {
 app.get("/:id", async (req, res) => {
 
     console.log(req.params.id)
-   async function webscraping() {
+    async function webscraping() {
         //Raspagem de dados no site www.ncrangola.com
-   async function NCRANGOLA() {
-    await axios.get(`https://www.ncrangola.com/loja/particulares/pt/pesquisa?controller=search&orderby=position&orderway=desc&search_query=${req.params.id}&submit_search=`)
-    .then((result) => {
-        
-        const $ = cheerio.load(result.data)
+        async function NCRANGOLA() {
+            await axios.get(`https://www.ncrangola.com/loja/particulares/pt/pesquisa?controller=search&orderby=position&orderway=desc&search_query=${req.params.id}&submit_search=`)
+                .then((result) => {
 
-        $('.produto').each( (index, element) => {
+                    const $ = cheerio.load(result.data)
 
-            const imgLink = $(element)
+                    $('.produto').each((index, element) => {
+                        const id = indexGlobal
+
+
+                        const imgLink = $(element)
                             .find(' div.produto_img > div.produto_img_in.produto_img_desktop_bt > span > a > img')
                             .attr('src')
 
-            const marca = $(element)
-                        .find(' div.produto_info > div > h6')
-                        .text()
-            
-            const productLink = $(element)
-                        .find(' div.produto_info > div > a')
-                        .attr('href')
-            
-            const desc = $(element)
-                        .find(' div.produto_info > div > a > h5')
-                        .text()
-                            
-
-            const price = $(element)
-                            .find('div.produto_info > div > div > div > span')
-                            .text()
-                            
-            
-            product [index] = {marca, imgLink, productLink, desc, price}
-            indexGlobal++;
-        })
-        
-    })
-    .catch((err) => {
-        console.log("Erro ao executar",err)
-});
- }
-
-    async function QUERRAPIDOANGOLA() {
-    
-    await axios.get(`https://www.querapidoangola.com/?s=${req.params.id}&post_type=product`)
-    .then((result) => {
-        
-        const $ = cheerio.load(result.data)
-
-        $('.col-inner').each( (index, element) => {
-            
-            const imgLink = $(element)
-                            .find('  div.image-none > a > img')
-                            .attr('srcset')
-
-            const marca = $(element)
-                            .find(' div.title-wrapper > p.category')
-                            .text()
-            const desc = $(element)
-                            .find('p.product-title > a')
+                        const marca = $(element)
+                            .find(' div.produto_info > div > h6')
                             .text()
 
-            const productLink = $(element)
-                            .find(' p.product-title > a')
+                        const productLink = $(element)
+                            .find(' div.produto_info > div > a')
                             .attr('href')
 
-            const price = $(element)
-                            .find('div.price-wrapper > span.price > span.woocommerce-Price-amount > bdi')
+                        const desc = $(element)
+                            .find(' div.produto_info > div > a > h5')
                             .text()
-            
-                            
-            
-            product[indexGlobal] = {marca, imgLink, productLink, desc, price}
-            indexGlobal++;
-        })
-        console.log(product)
-        
-    })
-    .catch((err) => {
-        console.log("Erro ao executar",err)
-});
- 
-}
 
-await NCRANGOLA()
-await QUERRAPIDOANGOLA()
-    
+
+                        const price = $(element)
+                            .find('div.produto_info > div > div > div > span')
+                            .text()
+
+
+                        product[index] = {
+                            id,
+                            marca,
+                            imgLink,
+                            productLink,
+                            desc,
+                            price
+                        }
+                        indexGlobal++;
+                    })
+
+                })
+                .catch((err) => {
+                    console.log("Erro ao executar", err)
+                });
+        }
+
+        async function QUERAPIDOANGOLA() {
+            await axios.get(``)
+                .then((result) => {
+
+                    const $ = cheerio.load(result.data)
+
+                    $('.produto').each((index, element) => {
+                        const id = indexGlobal
+
+
+                        const imgLink = $(element)
+                            .find(' div.produto_img > div.produto_img_in.produto_img_desktop_bt > span > a > img')
+                            .attr('src')
+
+                        const marca = $(element)
+                            .find(' div.produto_info > div > h6')
+                            .text()
+
+                        const productLink = $(element)
+                            .find(' div.produto_info > div > a')
+                            .attr('href')
+
+                        const desc = $(element)
+                            .find(' div.produto_info > div > a > h5')
+                            .text()
+
+
+                        const price = $(element)
+                            .find('div.produto_info > div > div > div > span')
+                            .text()
+
+
+                        product[index] = {
+                            id,
+                            marca,
+                            imgLink,
+                            productLink,
+                            desc,
+                            price
+                        }
+                        indexGlobal++;
+                    })
+
+                })
+                .catch((err) => {
+                    console.log("Erro ao executar", err)
+                });
+        }
+
+
+
+        await NCRANGOLA()
+        await QUERAPIDOANGOLA()
+
 
     }
-   await  webscraping()
+    await webscraping()
     await res.status(200).json(product)
 
-    
+
 
 })
 
 app.listen(8085, () => {
 
 })
-
-
-    
-
-
-
-
-
